fc3ef7ad63e0cc9c7b440c720f873124d404b42e
ozzyofficial <oziomaobidinma3@gmail.com>
2024-06-25 09:25:55 +0100

    added new exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,9 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
+import projectFour from "./images/projects/projectFour.jpg";
+import projectFive from "./images/projects/projectFive.jpg";
+import projectSix from "./images/projects/projectSix.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
@@ -14,6 +17,9 @@ export {
   projectOne,
   projectTwo,
   projectThree,
+  projectFour,
+  projectFive,
+  projectSix,
   testimonialOne,
   testimonialTwo,
   quote,
