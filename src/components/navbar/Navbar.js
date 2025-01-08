10e6324ede375a73c563f0de51e4e64476e3a776
Paul Obidinma <oziomaobidinma3@gmail.com>
2025-01-08 23:31:11 +0100

    Changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
-        <img src={logo} alt="logo" width="100" height="100" />
+        <img src={logo} alt="logo" width="100" height="50" />
       </div>
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
