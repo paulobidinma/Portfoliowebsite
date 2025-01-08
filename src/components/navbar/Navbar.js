3fd40be47a27bf5cba3653bde80b7df68a3bfd9f
Paul Obidinma <oziomaobidinma3@gmail.com>
2025-01-08 23:35:09 +0100

    Increase navbar height for improved layout

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,7 +9,7 @@ import { navLinksdata } from '../../constants';
 const Navbar = () => {
   const [showMenu, setShowMenu]=useState(false)
   return (
-    <div className="w-full h-18 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
+    <div className="w-full h-20 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
         <img src={logo} alt="logo" width="100" height="50" />
       </div>
